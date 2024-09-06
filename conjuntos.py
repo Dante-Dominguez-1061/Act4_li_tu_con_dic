91f5b87bc62f583802f5f221cc20a273221e06f5
Dante-Dominguez-1061 <164534070+Dante-Dominguez-1061@users.noreply.github.com>
2024-09-06 12:18:43 -0600

    Add files via upload

---
+++ conjuntos.py
@@ -0,0 +1,10 @@
+misamigos = {"Luis","David","Oscar"}
+print(misamigos)
+print("Longitud mis amigos")
+print(len(misamigos))
+print("Elementos de mis amgigos")
+lista = list(misamigos)
+print(lista[2])
+print("Uso de for en mis amigos")
+for listadeamigos in misamigos:
+    print(listadeamigos)
---
+++ diccionario.py
@@ -0,0 +1,14 @@
+estudiantes = {
+    "ID": 1061,
+    "Nombre": "Juan",
+    "Edad": 16,
+    "Genero": "Masculino",
+    "Especialidad": "Programacion",
+    "Email": "juan@gmail.com"
+}
+print(estudiantes)
+print("Elementos de estudiantes")
+print(estudiantes["Nombre"])
+print("Uso de for en estudiantes")
+for datos in estudiantes:
+    print(estudiantes[datos])
